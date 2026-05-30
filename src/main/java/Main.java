13a6b39b3078b7c57917e7dad8bb622ae2e80213
Abhishek S <thisisabhishek.78@gmail.com>
2026-05-30 10:52:17 +0530

    replaced multiple if statements with switch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,31 +3,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
 //        REPL: read-eval-print-loop
         while (true) {
             System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
+
+            String command = scanner.nextLine(),
+                    firstWord = !command.contains(" ") ? command : command.substring(0, command.indexOf(" ")),
+                    remaining = !command.contains(" ") ? "" : command.substring(command.indexOf(" ") + 1);
 
 //            exit command
-            if (input.equalsIgnoreCase("exit")) {
-                break;
-            } else if (input.startsWith("echo")) {
+            switch (firstWord) {
+                case "exit":
+                    System.exit(0);
+                case "echo":
 //                echo command
-                System.out.println(input.substring(5));
-            } else if (input.startsWith("type")) {
+                    System.out.println(remaining);
+                    break;
+                case "type":
 //                type command
-                if (input.substring(5).equalsIgnoreCase("echo")) {
-                    System.out.println("echo is a shell builtin");
-                } else if (input.substring(5).equalsIgnoreCase("exit")) {
-                    System.out.println("exit is a shell builtin");
-                } else if (input.substring(5).equalsIgnoreCase("type")) {
-                    System.out.println("type is a shell builtin");
-                } else {
-                    System.out.println(input.substring(5) + ": not found");
-                }
-            } else {
-                System.out.println(input + ": command not found");
+                    boolean isShellBuiltin = remaining.equals("exit") || remaining.equals("echo") || remaining.equals("type");
+                    if (isShellBuiltin)
+                        System.out.println(remaining + " is a shell builtin");
+                    else System.out.println(remaining + ": not found");
+                    break;
+                default:
+                    System.out.println(command + ": command not found");
             }
         }
     }
